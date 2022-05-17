8a2aa3cab3643ecc659181a00108726256a51988
ChloeLvsr <ch.leservoisier@gmail.com>
2022-05-17 10:20:38 +0100

    integration proposition trajet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CguComponent } from './components/pages/cgu/cgu.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { InscriptionComponent } from './components/pages/inscription/inscription.component';
+import { PropositionTrajetComponent } from './components/pages/proposition-trajet/proposition-trajet.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'inscription', component: InscriptionComponent },
-  { path: 'cgu', component: CguComponent }
+  { path: 'cgu', component: CguComponent },
+  { path: 'proposition-trajet', component:PropositionTrajetComponent }
   
 ];
 
